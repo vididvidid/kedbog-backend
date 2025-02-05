f68f32fed50183b996bab0b878db94b98a86a7cf
Yash kumar kasaudhan <vididvidid@gmail.com>
2025-02-06 03:13:21 +0530

    port change

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ io.on('connection', (socket) => {
   });
 });
 // Start Server using the HTTP server.
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
